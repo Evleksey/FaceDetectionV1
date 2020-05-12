1042b038926ed21308521692836b4ae30acf9cb5
Алексей Евграшин <m1803525@edu.misis.ru>
2020-05-12 22:19:43 +0300

    Started work on training code

--- DetectionTrain/Program.cs
+++ DetectionTrain/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+using FaceDetectionV1;
+using Image = FaceDetectionV1.Image;
 
 namespace DetectionTrain
 {
@@ -10,6 +14,14 @@ namespace DetectionTrain
     {
         static void Main(string[] args)
         {
+            int name = 0;
+            while (File.Exists($"img{name}.bmp"))
+            {
+
+                Image img = new Image(new Bitmap($"img{name}.bmp"));
+                IntegralImage integral = new IntegralImage(img);
+
+            }
         }
     }
 }
